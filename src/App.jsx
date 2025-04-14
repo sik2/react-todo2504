ec3d6faf7ac3fa74c3ada757fe792456617c33fa
sik2 <bickwang123@gmail.com>
2025-04-14 20:04:58 +0900

    init ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 function App() {
   return (
     <>
-      <h1>hello world!</h1>
+      <h3>todo app</h3>
+      <input type="text" /> <button>입력</button>
+      <ul>
+        <li>
+          <input type="checkbox" /> 리액트 공부하기
+          <button>X</button>
+        </li>
+        <li>
+          <input type="checkbox" /> 리액트 공부하기
+          <button>X</button>
+        </li>
+        <li>
+          <input type="checkbox" /> 리액트 공부하기
+          <button>X</button>
+        </li>
+      </ul>
     </>
   );
 }
