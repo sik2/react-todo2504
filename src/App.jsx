70d3cfbf8ec8254a3698a7a5ef9699e166c2836c
sik2 <bickwang123@gmail.com>
2025-04-14 20:49:21 +0900

    input

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
+import { useState } from "react";
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = useState([
     { id: 1, text: "리액트 공부하기", checked: false },
     { id: 2, text: "운동하기", checked: true },
     { id: 3, text: "리액트 복습하기", checked: false },
-  ];
+  ]);
+
+  let lastId = todos.length + 1;
+
+  const onInsert = (text) => {
+    const newTodos = [...todos, { id: lastId, text, checked: false }];
+    setTodos(newTodos);
+    lastId++;
+  };
 
   return (
     <>
       <h3>todo app</h3>
-      <TodoForm />
+      <TodoForm onInsert={onInsert} />
       <TodoList todos={todos} />
     </>
   );
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,7 +1,23 @@
-function TodoForm() {
+import { useState } from "react";
+
+function TodoForm({ onInsert }) {
+  const [text, setText] = useState("");
+
+  const onChangeSubmmit = (e) => {
+    e.preventDefault(); // 기본 동작을 막음
+    onInsert(text);
+  };
+
+  const onChangeInput = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <>
-      <input type="text" /> <button>입력</button>
+      <form onSubmit={onChangeSubmmit}>
+        <input type="text" onChange={onChangeInput} />
+        <button type="submit">입력</button>
+      </form>
     </>
   );
 }
